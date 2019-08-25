83c2574c4ea1e21602458096702a688bd689a021
vferrucci <54486291+vferrucci@users.noreply.github.com>
2019-08-25 18:15:27 -0400

    Add files via upload

---
+++ Coin.java
@@ -0,0 +1,40 @@
+/**
+*@author: Veranika Tolkach
+*Assignment 4
+* The class Coin simulates a coin.
+*/
+import java.util.Random;
+public class Coin
+{
+private String sideUp;
+   
+   /**the default constructor to set up 
+   the initial side of the coin */
+   public Coin()
+   {
+   toss();
+   }//end default constructor
+   
+   /**The toss method simulates the tossing
+   of the coin*/
+   public void toss()
+   {
+   Random rand=new Random();
+   if(rand.nextInt(2)==0)
+   {
+      sideUp="tails";
+   }
+   else
+   {
+      sideUp="heads";
+   }
+   }//end toss
+   
+   /*The method getSideUp returns the value
+   of the side*/
+   public String getSideUp()
+   {
+   return sideUp;
+   }//end getSideUp
+
+}//end Coin
---
+++ CoinDemo.java
@@ -0,0 +1,41 @@
+/**
+*@author: Veranika Tolkach
+*Assignment 4
+* This program simulates tossing of a coin.
+*First, it calls the default constructor to 
+*assign the initial side to the coin.
+*Then the program tosses the coin 20 times,
+*and displays which side the coin is facing up each time.
+*After that the program displays the number of times
+*each side appeared.
+*/
+
+public class CoinDemo
+{
+   public static void main (String[] args)
+   {
+   String side;
+   int count=0, count2=0;
+   Coin coinSide=new Coin();//calls the default constructor
+   System.out.println("The coin is initially facing " + coinSide.getSideUp() +"\n");
+   
+   //Toss the coin 20 times
+   System.out.println("Tossing the coin 20 times");
+   for (int i=0; i<20; i++)
+   {
+   coinSide.toss();
+   side =coinSide.getSideUp();
+      if(side=="heads")
+      {
+         count++;//calculates the number of "heads"
+      }
+      else
+      {
+         count2++;//calculates the number of "tails"
+      }
+   System.out.println(side);
+   }//end for
+   System.out.println("The program displayed heads " +count+ " times.");
+   System.out.println("The program displayed tails " +count2+ " times.");
+   }//end main
+}//end CoinDemo
